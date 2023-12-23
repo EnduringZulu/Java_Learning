74e66a7d4dafa539a7be7600b3349717da22c93e
EnduringZulu <mingrattikorn@gmail.com>
2023-12-23 22:12:10 +0700

    for-each-loop

--- src/main.java
+++ src/main.java
@@ -294,7 +294,22 @@ public class main {
 //		System.out.println(grocerylist.get(0).get(0)); //choose bakerylist and inside choose choco_cake
 		
 		
-		//for-each=
+		//for-each =	traversing technique to iterate through the elements in an array/collection
+		//				less steps, more readable
+		//				less flexible
+		
+		//String[] animals = {"cat","dog","rat","bird"};
+//		ArrayList<String> animals = new ArrayList<String>();
+//		
+//		animals.add("cat");
+//		animals.add("dog");
+//		animals.add("rat");
+//		animals.add("bird");
+//		
+//		for(String i : animals) {
+//			System.out.println(i);
+//		}
+		
 		
 	}
 }
